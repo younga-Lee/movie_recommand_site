291555be3f2764ccc9309b2bb4d236dbc012e7b9
hyuna333 <kook980316@gmail.com>
2023-05-19 15:11:02 +0900

    평점이 높은 순으로 영화 20개 받아와서 보여주기

---
+++ front-server/src/components/MainView/TopCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img id="imglist" :src="url + movie.poster_path" alt="poster" @click="goDetail">
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  name: 'TopCard',
+  data() {
+    return {
+      url : 'https://image.tmdb.org/t/p/w600_and_h900_bestv2',
+    }
+  },
+  props: {
+    movie: Object,
+  },
+  methods: {
+    goDetail() {
+      this.$router.push({name: 'detail', params: {id:`${this.movie.id}`}})
+    }
+  }
+}
+</script>
+
+<style>
+#imglist {
+  width: 200px;
+  margin-right: 20px;
+}
+</style>
