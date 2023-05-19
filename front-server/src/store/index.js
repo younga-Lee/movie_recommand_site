7b88accd664c164ef2c035b1194c71d628b4823a
hyuna333 <kook980316@gmail.com>
2023-05-19 15:15:30 +0900

    평점 높은 영화 리스트 받아오는 action 작성

--- front-server/src/store/index.js
+++ front-server/src/store/index.js
@@ -8,6 +8,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     boxoffieList : [],
+    toprateList : [],
+    TMDB_API_KEY : process.env.VUE_APP_TMDB_API_KEY,
   },
   getters: {
   },
@@ -15,19 +17,36 @@ export default new Vuex.Store({
     GET_DATA(state, movies){
       state.boxoffieList = movies
     },
+    GET_TOP(state, movies){
+      state.toprateList = movies
+    },
   },
   actions: {
     getBox(context) {
-      const TMDB_API_KEY = process.env.VUE_APP_TMDB_API_KEY
       
       axios({
         method: 'get',
-        url : `https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}`,
+        url : `https://api.themoviedb.org/3/movie/now_playing?api_key=${this.state.TMDB_API_KEY}`,
       })
       
       .then((res) => {
         // console.log(res.data.results)
         context.commit('GET_DATA', res.data.results)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+    getTop(context) {
+      
+      axios({
+        method: 'get',
+        url : `https://api.themoviedb.org/3/movie/top_rated?api_key=${this.state.TMDB_API_KEY}`,
+      })
+      
+      .then((res) => {
+        // console.log(res.data.results)
+        context.commit('GET_TOP', res.data.results)
       })
       .catch((err) => {
         console.log(err)
