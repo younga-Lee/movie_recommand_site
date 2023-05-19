fc74d6f18530f26435298e40fdf8ae32e0e5d184
hyuna333 <kook980316@gmail.com>
2023-05-19 15:18:13 +0900

    영화 디테일 정보와 출연진 정보를 받아서 MovieDetail로 넘겨주는 로직 작성

--- front-server/src/views/DetailView.vue
+++ front-server/src/views/DetailView.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <MovieDetail 
-    :movie='movie'
-    :actors='actors'
+    :movie="movie"
+    :actors="actors"
+    :videoId="videoId"
+    :thumbUrl="thumbUrl"
     />
   </div>
 </template>
@@ -18,9 +20,12 @@ export default {
   },
   data() {
     return {
-      movie: [],
+      movie: {},
       actors: [],
+      videoId: '',
+      thumbUrl: '',
       TMDB_API_KEY: process.env.VUE_APP_TMDB_API_KEY,
+      YOUTUBE_API_KEY: process.env.VUE_APP_YOUTUBE_API_KEY,
     }
   },
   computed: {
@@ -35,10 +40,12 @@ export default {
       axios({
         method: 'get',
         url: `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${this.TMDB_API_KEY}`,
+        params: {language: 'ko'},
       })
       .then((res) => {
         // console.log(res.data)
         this.movie = res.data
+        // this.getVideo()
       })
       .catch((err) => {
         console.log(err)
@@ -49,41 +56,46 @@ export default {
 
       axios({
         method: 'get',
-        url: `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${this.TMDB_API_KEY}`
-      })
+        url: `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${this.TMDB_API_KEY}`,
+        params: {language: 'ko'},
+        })
       .then((res) => {
-        console.log(res.data.cast.slice(0, 5))
+        // console.log(res.data.cast.slice(0, 5))
         this.actors = res.data.cast.slice(0, 5)
       })
       .catch((err) => {
         console.log(err)
       })      
-    }
+    },
+    getVideo(){
+
+      axios({
+        method: 'get',
+        url: `https://www.googleapis.com/youtube/v3/search?key=${this.YOUTUBE_API_KEY}`,
+        params: {
+          part: 'snippet',
+          type: 'video',
+          q: `${this.movie.title + ' 예고'}`
+        }
+      })
+      .then((response) => {
+        console.log(response.data.items[0])
+        this.videoId = response.data.items[0].id.videoId
+        this.thumbUrl = response.data.items[0].snippet.thumbnails.default.url
+        // this.url = `https://www.youtube.com/embed/${videoId}`
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }    
   },
   created() {
     this.getDetail()
     this.getActors()
-  }
+  },
 }
 </script>
 
 <style>
-#back {
-  height: 500px;
-}
 
-#bgimage {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  opacity: 0.5;
-}
-
-#poster {
-  width: 200px;
-}
-
-.actors img {
-  width: 50px;
-}
 </style>
