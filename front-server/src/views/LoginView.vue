918fac0e5b821e0e40c14b079e1b1f604ce2f3a6
hyuna333 <kook980316@gmail.com>
2023-05-19 17:49:35 +0900

    login, sugnup form 제작

--- front-server/src/views/LoginView.vue
+++ front-server/src/views/LoginView.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-
+    <form id="loginform">
+      <div class="row mb-3">
+        <label for="inputEmail4" class="form-label">ID</label>
+        <input type="email" class="form-control" id="inputEmail4">
+      </div>
+      <div class="row mb-3">
+        <label for="inputPassword4" class="form-label">Password</label>
+        <input type="password" class="form-control" id="inputPassword4">
+      </div>
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
   </div>
 </template>
 
@@ -11,5 +21,15 @@ export default {
 </script>
 
 <style>
+#loginform {
+  width: 500px;
+  height: 500px;
+  margin: auto;
+  margin-top: 100px;
+}
+
+label {
+  text-align: left;
+}
 
 </style>
