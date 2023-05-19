cf2b947e694282debc271350a00151eaa4425f77
hyuna333 <kook980316@gmail.com>
2023-05-19 15:19:12 +0900

    평점 높은 영화 리스트를 보여주는 TopCard와 연결

--- front-server/src/views/MainView.vue
+++ front-server/src/views/MainView.vue
@@ -3,7 +3,16 @@
     <h1># 현재 상영작</h1>
     <div class="container">
       <BoxCard 
-      v-for="movie in movies"
+      v-for="movie in boxmovies"
+      :key="movie.id"
+      :movie="movie"
+      />
+    </div>
+    <br><br>
+    <h1># 다른 사람들이 즐긴 영화</h1>
+    <div class="container">
+      <TopCard 
+      v-for="movie in topmovies"
       :key="movie.id"
       :movie="movie"
       />
@@ -13,24 +22,34 @@
 
 <script>
 import BoxCard from '@/components/MainView/BoxCard'
+import TopCard from '@/components/MainView/TopCard'
 
 export default {
   name: 'MovieView',
   methods: {
     getBox() {
       this.$store.dispatch('getBox')
-    }
+    },
+    getTop() {
+      this.$store.dispatch('getTop')
+    },
   },
   created() {
     this.getBox()
+    this.getTop()
   },
   computed: {
-    movies() {
+    boxmovies() {
       return this.$store.state.boxoffieList
-    }
+    },
+    topmovies() {
+      return this.$store.state.toprateList
+    },
+
   },
   components: {
-    BoxCard
+    BoxCard,
+    TopCard
   }
 }
 </script>
