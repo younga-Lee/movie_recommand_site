88c4f5f9daf14eb85c6bea272600efcdae1b6a7e
이영아 <dktjsp24@gmail.com>
2023-05-24 11:26:34 +0900

    follow

--- finalproject/movies/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-# from movies import views
-
-app_name = 'movies'
-urlpatterns = [
-    # path('movies/', views.movie_list),
-    # path('movies/<int:movie_id>/', views.movie_detail),
-]
