b43234b0e3e51e9e8eb05faab56929602df67d4e
hyuna333 <kook980316@gmail.com>
2023-05-24 11:25:46 +0900

    유저 정보 받아오는 path 구현

--- finalproject/accounts/views.py
+++ finalproject/accounts/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated #로그인 해야만 가능
@@ -6,6 +7,7 @@ from .models import User
 from .serializers import UserProfileSerializer
 
 # Create your views here.
+@api_view(['GET'])
 def user_detail(request, username):
     User = get_user_model()
     user = get_object_or_404(User, username=username)
