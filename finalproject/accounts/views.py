393d41e5f469439435e64175feb1704029468972
hyuna333 <kook980316@gmail.com>
2023-05-24 15:41:11 +0900

    유저 구분해서 profile 보여주는 작업 중

--- finalproject/accounts/views.py
+++ finalproject/accounts/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated #로그인 해야만 가능
@@ -15,6 +15,7 @@ def user_detail(request, username):
     return Response(serializer.data)   
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def follow(request, username):
     person = get_object_or_404(User, username=username)
     me = request.user
