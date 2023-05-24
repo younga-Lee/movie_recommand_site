cd94ba0a0a66904a372303defd0674433234de9c
hyuna333 <kook980316@gmail.com>
2023-05-24 17:44:03 +0900

    팔로우 기능을 위해서 model 변경

--- finalproject/accounts/models.py
+++ finalproject/accounts/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     image = models.ImageField(upload_to='media/', blank=True, default='profile.png')
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+
+
