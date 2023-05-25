53786ec1ecdb66a5572a6560c1669d91a32d5433
hyuna333 <kook980316@gmail.com>
2023-05-25 14:00:43 +0900

    메인과 프로필 css 변경

--- front-server/src/views/ProfileView.vue
+++ front-server/src/views/ProfileView.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-    <div>
-      <div class="header">
-        <!-- <img src="@/assets/logo.png" alt="profile" id="profileimg"> -->
+    <div class="profilecontent">
+      <div class="profilehead">
+        <img :src="API_URL + profileuser.image" alt="profile" id="profileimg">
+        <div class="profiletext">
+          <p class="profilename">{{ profileuser.username }}</p>
+          <div class="followtext">
+            <p>follower <span>{{ profileuser.followers_cnt }}</span> followings <span>{{ profileuser.followings.length }}</span>
+            </p>
+          </div>
+        </div>
+        <img src="@/assets/editbutton.svg" alt="edit" @click="goEdit" class="editimg" v-if="loginuser.username == profileuser.username">
+        <img src="@/assets/followbtn.svg" alt="follow" @click="follow" class="editimg" 
+        v-if="loginuser.username != profileuser.username && !loginuser.followings.includes(profileuser.id)">
+      </div>
+      <!-- <div class="header">
         <img :src="API_URL + profileuser.image" alt="profile" id="profileimg">
         <div>
-          <!-- <p>{{ profileuser }}</p> -->
           <p>{{ profileuser.username }}</p>
-          <p>follower 2.3k  followings {{ profileuser.followings.length }}</p>
-          <button @click="follow" v-if="!loginuser.followings.includes(profileuser.id)">Follow</button>
-          <button @click="follow" v-if="loginuser.followings.includes(profileuser.id)">Unfollow</button>
+          <p>follower {{profileuser.followers_cnt}}  followings {{ profileuser.followings.length }}</p>
+          <div v-if="!loginuser.username == profileuser.username">
+            <button @click="follow" v-if="!loginuser.followings.includes(profileuser.id)">Follow</button>
+            <button @click="follow" v-if="loginuser.followings.includes(profileuser.id)">Unfollow</button>
+          </div>
           <button @click="goEdit" v-if="loginuser.username == profileuser.username">회원 정보 수정</button>
         </div>
-      </div>
+      </div> -->
       <div id="wishlist" class="border">
         <h2>WishList</h2>
         <div>
@@ -64,6 +77,7 @@ export default {
       })
       .then(() => {
         this.getLoginuser()
+        this.$router.go(this.$router.currentRoute)        
       })
       .catch((err) => {
         console.log(err)
@@ -89,6 +103,77 @@ export default {
 </script>
 
 <style>
+.profilecontent {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px 0px;
+  gap: 32px;
+
+  position: absolute;
+  width: 1440px;
+  height: 670px;
+  left: 0px;
+  top: 206px;
+}
+
+.profilehead {
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 0px;
+gap: 40px;
+
+width: 552px;
+height: 310px;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.profiletext {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: 12px;
+
+  width: 552px;
+  height: 80px;
+
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+}
+
+.profilename {
+  width: 552px;
+  height: 44px;
+
+  /* MO/36/Bold */
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 43px;
+
+  /* identical to box height, or 120% */
+  text-align: center;
+
+  /* starbucks/sys/light/on-secondary-fixed */
+  color: #00201A;
+
+  /* Inside auto layout */
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+}
+
 .header {
   text-align: left !important;
   display: flex;
@@ -96,10 +181,47 @@ export default {
 }
 
 #profileimg {
-  width: 200px;
+  width: 120px;
+  height: 120px;
 }
 
 #wishlist {
   width: 80%;
+}
+
+.followtext {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+
+  width: 111px;
+  height: 24px;
+
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+}
+
+.editimg {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 16px;
+
+  width: 127px;
+  height: 46px;
+
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
 }
 </style>
