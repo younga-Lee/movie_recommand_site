3569687bb90039e96a626676ce5fae9a7eead0cf
hyuna333 <kook980316@gmail.com>
2023-05-25 14:00:59 +0900

    영화 리뷰 수정하기 기능 추가

--- front-server/src/components/DetailView/MovieRated.vue
+++ front-server/src/components/DetailView/MovieRated.vue
@@ -20,10 +20,44 @@
         <input type="submit" value="등록하기">
       </form>
       <h3>코멘트 리스트</h3>
-      <div class="comment" v-for="comment in comments" :key="comment.id">
+      <div class="comment" v-for="comment in comments.slice().reverse()" :key="comment.id">
         <p>내용 : {{ comment.comment_content }}</p>
         <p>평점 : {{ comment.rating }}</p>
         <p>작성자 : {{ comment.username }}</p>
+        <button v-if="comment.username == loginuser.username" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#commentModal">
+          수정하기
+        </button>
+        <div class="modal fade" id="commentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="commentModalLabel">코멘트 수정하기</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form @submit.prevent="editComment(comment.id)">
+                  <label for="comment" class="form-label"><h3>코멘트</h3></label>
+                  <div class="star-rating space-x-4 mx-auto">
+                    <input type="radio" id="5-edits" name="rating" value="5" v-model="editratings"/>
+                    <label for="5-edits" class="star pr-4">★</label>
+                    <input type="radio" id="4-edits" name="rating" value="4" v-model="editratings"/>
+                    <label for="4-edits" class="star">★</label>
+                    <input type="radio" id="3-edits" name="rating" value="3" v-model="editratings"/>
+                    <label for="3-edits" class="star">★</label>
+                    <input type="radio" id="2-edits" name="rating" value="2" v-model="editratings"/>
+                    <label for="2-edits" class="star">★</label>
+                    <input type="radio" id="1-edits" name="rating" value="1" v-model="editratings" />
+                    <label for="1-edits" class="star">★</label>
+                  </div>
+                  <textarea class="form-control" id="comment" rows="3" v-model="editcontent"></textarea>
+                  <button type="submit" class="btn btn-primary">수정하기</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 버튼 누르면 수정할 수 있는 모달이 나와서 form 제출하도록 만들기 -->
+        <button v-if="comment.username == loginuser.username" @click="deleteComment(comment.id)">삭제하기</button>
       </div>
     </div>
     <!-- {{ commentlist }} -->
@@ -40,6 +74,8 @@ export default {
     return {
       content: null,
       ratings: null,
+      editcontent: null,
+      editratings: null,
     }
   },
   props: {
@@ -47,11 +83,8 @@ export default {
     comments: Array,
   },
   computed: {
-    commentlist() {
-      // return this.props.comments.slice(0).reverse()
-      return this.props.comments.slice().sort((a, b) => {
-        return b.created_at - a.created_at
-      })
+    loginuser() {
+      return this.$store.state.loginuser
     }
   },
   methods: {
@@ -83,9 +116,45 @@ export default {
         console.log(err)
       })
     },
+    deleteComment(comment_pk) {
+      const movie_id = this.movie.id
+
+      axios({
+        method: 'delete',
+        url: `${API_URL}/api/v1/movies/${movie_id}/comments/${comment_pk}/`,
+        headers: {
+          Authorization: `Token ${this.$store.state.token}`
+        }       
+      })
+      .then(() => {
+        this.$router.go(this.$router.currentRoute)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+    editComment(comment_pk) {
+      const movie_id = this.movie.id
+      const rating = this.editratings
+      const comment_content = this.editcontent
+
+      axios({
+        method: 'PUT',
+        url: `${API_URL}/api/v1/movies/${movie_id}/comments/${comment_pk}/`,
+        data: { rating, comment_content },
+        headers: {
+          Authorization: `Token ${this.$store.state.token}`
+        }         
+      })
+      .then(() => {
+        this.$router.go(this.$router.currentRoute)
+      })
+      .catch((err) => {
+        console.log(err)
+        console.log(comment_pk)
+      })
+    }
   },
-  created() {
-  }
 }
 </script>
 
