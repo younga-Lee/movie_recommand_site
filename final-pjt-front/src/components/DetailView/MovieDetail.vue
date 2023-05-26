c1013075a59750b6f85685596e9532b743c3aa4f
hyuna333 <kook980316@gmail.com>
2023-05-26 11:35:25 +0900

    영화 상세 페이지 css 변경

--- final-pjt-front/src/components/DetailView/MovieDetail.vue
+++ final-pjt-front/src/components/DetailView/MovieDetail.vue
@@ -2,45 +2,44 @@
   <div>
     <div id="back">
       <img id="bgimage" :src="imgurl + movie.backdrop_path" alt="backimage">
+      <div class="trailer-container">
+        <iframe :src="videourl + videoId" frameborder="0" height="351" width="625">영상</iframe>
+      </div>
     </div>
 
-    <div id="front">
-      <div class="container">
-        <img class="col-2" id="poster" :src="imgurl + movie.poster_path" alt="poster">
-        <div class="ptag col-7">
-          <div>
-            <h2 style="font-weight: bold">{{ movie.title }}</h2>
-            <button @click="addWish(movie.id)">영화 주문하기</button>
-            <button @click="addWish(movie.id)">영화 주문취소</button>
-          </div>
-          <p>
-            <span>평점 : ★{{ (movie.vote_average / 2).toFixed(1) }}</span> • 
-            <span>{{ genres ? genres[0]?.name : '' }}/{{ genres ? genres[1]?.name : '' }}/{{ genres ? genres[2]?.name : '' }}</span>
-          </p>
-          <p>줄거리</p>
+    <div class="container">
+      <img id="poster" :src="imgurl + movie.poster_path" alt="poster">
+      <div class="ptag">
+        <div class="detailtitle">
+          <p class="movietitle">{{ movie.title }}</p>
+          <button v-if="!loginuser.likes_movies.includes(movie.id)" @click="addWish(movie.id)"><p>영화 주문하기</p></button>
+          <button v-if="loginuser.likes_movies.includes(movie.id)" @click="addWish(movie.id)"><p>영화 주문취소</p></button>
+        </div>
+        <hr>
+        <p class="rateavg">평점 : ★{{ (movie.vote_average / 2).toFixed(1) }}</p>
+        <!-- <p>
+          <span></span> • 
+          <span>{{ genres ? genres[0]?.name : '' }}/{{ genres ? genres[1]?.name : '' }}/{{ genres ? genres[2]?.name : '' }}</span>
+        </p> -->
+        <div class="description">
+          <p class="plot">줄거리</p>
           <p class="overview">{{ movie.overview }}</p>
         </div>
-        <div class="col" id="trailer">
-          <iframe :src="videourl + videoId" frameborder="0" height="200" width="300">영상</iframe>
+      </div>
+    </div>
 
+    <div id="cast">
+      <div class="casttitle">
+        <p>출연진</p>
+        <hr>
+      </div>
+      <div class="castgroup">
+        <div class="person" v-for="(actor, index) in actors" :key="index">
+          <img :src="imgurl + actor.profile_path" alt="actorimg">
+          <p>{{ actor.name }}</p>
         </div>
       </div>
-      <br>
-      <div id="mid">
-        <div id="cast">
-          <h3 class="actorlist">출연진</h3>
-          <div class="card-group">
-            <div class="card" v-for="(actor, index) in actors" :key="index">
-              <img :src="imgurl + actor.profile_path" alt="actorimg">
-              <span>{{ actor.name }}</span>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-
-    </div>    
+    </div>   
   </div>
 </template>
 
@@ -91,6 +90,9 @@ export default {
     },
     username() {
       return this.$store.state.username
+    },
+    loginuser() {
+      return this.$store.state.loginuser
     }
   },
   created() {
@@ -101,32 +103,211 @@ export default {
 
 <style>
 #back {
-  height: 300px;
-  position: relative;
-  /* z-index: 1; */
+height: 514px;
+left: 0px;
+top: 206px;
+/* position: relative; */
+}
+
+.trailer-container {
+  position: absolute;
+  left: calc(50% - 625px/2 + 0.5px);
+  top: 288px;
 }
 
 #bgimage {
-  width: 100%;
-  height: 130%;
+  width: 120%;
+  height: 100%;
   object-fit: cover;
   opacity: 0.5;
 }
 
 #poster {
-  height: 300px;
+width: 212px;
+height: 294px;
+flex: none;
+order: 0;
+flex-grow: 0;
+border-radius: 8px;
 }
 
 #front {
-  position: relative;
-  /* z-index: 1; */
-  margin-left: 20px;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: flex-start;
+padding: 60px 156px;
+gap: 24px;
+
+/* position: absolute; */
+width: 1440px;
+height: 414px;
+left: 0px;
+top: 720px;
 }
 
 .container {
-  margin: 0;
-  width: 100%;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: flex-start;
+padding: 60px 156px;
+gap: 24px;
+
+margin: 0;
+margin-bottom: 0;
+/* position: absolute; */
+/* width: 1440px; */
+height: 414px;
+left: 0px;
+top: 720px;
+
 }
+
+#cast {
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 40px;
+
+width: 1128px;
+height: 178px;
+
+margin: auto;
+}
+
+.castgroup {
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 0px;
+gap: 8px;
+
+width: 1128px;
+height: 80px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+}
+
+.person {
+  display: flex;
+flex-direction: row;
+align-items: center;
+padding: 16px 12px;
+gap: 16px;
+
+width: 219.2px;
+height: 80px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 1;
+}
+
+.person img {
+width: 48px;
+height: 48px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+border-radius: 50%
+}
+
+.person p {
+width: 131.2px;
+height: 48px;
+
+/* MO/16/Regular */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+/* or 150% */
+
+color: #000000;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 1;
+}
+
+.casttitle {
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 24px;
+
+width: 1128px;
+height: 58px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.casttitle p {
+width: 67px;
+height: 32px;
+
+/* MO/24/Bold */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 32px;
+/* identical to box height, or 133% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.casttitle hr {
+width: 1128px;
+height: 2px;
+
+/* starbucks/ref/neutral/neutral87 */
+
+background: #DBDAD3;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+}
+
 .card-group img {
   width: 70px;
   text-align: center;
@@ -160,24 +341,184 @@ export default {
 }
 
 .ptag {
-  margin-top: 70px;
-  display: flex;
-  flex-direction: column;
-  justify-content: end;
-  text-align: left;
-  margin-left: 20px;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 24px;
+
+width: 864px;
+height: 293px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
 }
 
-.ptag p {
-  margin: 8px;
+.detailtitle {
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 0px;
+gap: 16px;
+
+width: 864px;
+height: 43px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+}
+
+.detailtitle button {
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 12px 18px;
+gap: 6px;
+
+width: 117px;
+height: 43px;
+
+background: #3C4043;
+/* Shadow/XSM */
+
+box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.04);
+border-radius: 100px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+
+button p {
+width: 81px;
+height: 19px;
+margin-bottom: 0;
+
+/* MO/14/bold */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 14px;
+line-height: 19px;
+/* identical to box height, or 133% */
+
+/* Base/White */
+color: #FFFFFF;
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.movietitle {
+  width: 210px;
+height: 32px;
+
+/* MO/24/Bold */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 32px;
+/* identical to box height, or 133% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.description {
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 8px;
+
+width: 864px;
+height: 152px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 3;
+flex-grow: 0;
+}
+
+.plot {
+width: 864px;
+height: 24px;
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 24px;
+/* identical to box height, or 150% */
+
+
+/* starbucks/ref/neutral/neutral50 */
+
+color: #777771;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
 }
 
 .overview {
-  overflow: hidden;
-  /* text-overflow: ellipsis; */
-  /* display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical; */
+overflow: hidden;
+text-overflow: ellipsis;
+display: -webkit-box;
+-webkit-line-clamp: 4;
+-webkit-box-orient: vertical;
+width: 864px;
+height: 91px;
+
+/* MO/16/Regular */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+/* or 150% */
+
+/* starbucks/ref/neutral/neutral50 */
+color: #777771;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
 }
 
 #mid {
@@ -186,5 +527,52 @@ export default {
 
 #trailer img {
   width: 200px;
+}
+
+.rateavg {
+width: 864px;
+height: 24px;
+
+/* MO/16/Regular */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+/* identical to box height, or 150% */
+
+
+/* starbucks/ref/neutral/neutral50 */
+
+color: #777771;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 2;
+flex-grow: 0;  
+}
+
+.container hr {
+width: 864px;
+height: 2px;
+
+/* starbucks/ref/neutral/neutral50 */
+background: #777771;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;  
+}
+</style>
+
+<style scoped>
+p {
+  margin-bottom: 0;
 }
 </style>
--- final-pjt-front/src/components/DetailView/MovieRated.vue
+++ final-pjt-front/src/components/DetailView/MovieRated.vue
@@ -1,67 +1,87 @@
 <template>
   <div>
-    <div id="rateform">
-      <h2>평점 남기기</h2>
-      <form @submit.prevent="createComment" class="commentform">
-        <label for="comment" class="form-label"><h3>코멘트</h3></label>
-        <div class="star-rating space-x-4 mx-auto">
-          <input type="radio" id="5-stars" name="rating" value="5" v-model="ratings"/>
-          <label for="5-stars" class="star pr-4">★</label>
-          <input type="radio" id="4-stars" name="rating" value="4" v-model="ratings"/>
-          <label for="4-stars" class="star">★</label>
-          <input type="radio" id="3-stars" name="rating" value="3" v-model="ratings"/>
-          <label for="3-stars" class="star">★</label>
-          <input type="radio" id="2-stars" name="rating" value="2" v-model="ratings"/>
-          <label for="2-stars" class="star">★</label>
-          <input type="radio" id="1-star" name="rating" value="1" v-model="ratings" />
-          <label for="1-star" class="star">★</label>
+    <form @submit.prevent="createComment" class="rateform">
+      <div class="commentform">
+        <div class="commenttitle">
+          <label for="comment" class="form-label">코멘트</label>
+          <hr>
         </div>
-        <textarea class="form-control" id="comment" rows="3" v-model="content"></textarea>
-        <input type="submit" value="등록하기">
-      </form>
-      <h3>코멘트 리스트</h3>
+
+        <div class="commentcontext">
+          <div class="star-rating space-x-4 mx-auto" id="stars">
+            <input type="radio" id="5-stars" name="rating" value="5" v-model="ratings"/>
+            <label for="5-stars" class="star pr-4">★</label>
+            <input type="radio" id="4-stars" name="rating" value="4" v-model="ratings"/>
+            <label for="4-stars" class="star">★</label>
+            <input type="radio" id="3-stars" name="rating" value="3" v-model="ratings"/>
+            <label for="3-stars" class="star">★</label>
+            <input type="radio" id="2-stars" name="rating" value="2" v-model="ratings"/>
+            <label for="2-stars" class="star">★</label>
+            <input type="radio" id="1-star" name="rating" value="1" v-model="ratings" />
+            <label for="1-star" class="star">★</label>
+          </div>
+          <textarea class="form-control" id="comment" rows="3" v-model="content"></textarea>
+        </div>
+      </div>
+      <button type="submit">
+        <p>평가하기</p>
+      </button>
+    </form>
+
+    <div class="commentlist">
+      <div class="cltitle">
+        <p>코멘트 리스트</p>
+        <hr>
+      </div>
+
       <div class="comment" v-for="comment in comments?.slice().reverse()" :key="comment.id">
-        <p>내용 : {{ comment.comment_content }}</p>
-        <p>평점 : {{ comment.rating }}</p>
-        <a @click="goProfile(comment.username)">{{ comment.username }}</a>
-        <p @click="goProfile(comment.username)">작성자 : {{ comment.username }}</p>
-        <button v-if="comment.username == loginuser.username" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#commentModal">
-          수정하기
-        </button>
-        <div class="modal fade" id="commentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="commentModalLabel">코멘트 수정하기</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <form @submit.prevent="editComment(comment.id)">
-                  <label for="comment" class="form-label"><h3>코멘트</h3></label>
-                  <div class="star-rating space-x-4 mx-auto">
-                    <input type="radio" id="5-edits" name="rating" value="5" v-model="editratings"/>
-                    <label for="5-edits" class="star pr-4">★</label>
-                    <input type="radio" id="4-edits" name="rating" value="4" v-model="editratings"/>
-                    <label for="4-edits" class="star">★</label>
-                    <input type="radio" id="3-edits" name="rating" value="3" v-model="editratings"/>
-                    <label for="3-edits" class="star">★</label>
-                    <input type="radio" id="2-edits" name="rating" value="2" v-model="editratings"/>
-                    <label for="2-edits" class="star">★</label>
-                    <input type="radio" id="1-edits" name="rating" value="1" v-model="editratings" />
-                    <label for="1-edits" class="star">★</label>
-                  </div>
-                  <textarea class="form-control" id="comment" rows="3" v-model="editcontent"></textarea>
-                  <button type="submit" class="btn btn-primary">수정하기</button>
-                </form>
+        <div class="commentone">
+          <a @click="goProfile(comment.username)">{{ comment.username }}</a>
+          <p>평점 : {{ comment.rating }}</p>
+        </div>
+
+        <div class="commenttwo">
+          <p>내용 : {{ comment.comment_content }}</p>
+          <button v-if="comment.username == loginuser.username" type="button" class="btn" data-bs-toggle="modal" data-bs-target="#commentModal">
+            수정
+          </button>
+          <button v-if="comment.username == loginuser.username" @click="deleteComment(comment.id)">삭제</button>
+          <div class="modal fade" id="commentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="commentModalLabel">코멘트 수정하기</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <form @submit.prevent="editComment(comment.id)">
+                    <label for="comment" class="form-label"><h3>코멘트</h3></label>
+                    <div class="star-rating space-x-4 mx-auto">
+                      <input type="radio" id="5-edits" name="rating" value="5" v-model="editratings"/>
+                      <label for="5-edits" class="star pr-4">★</label>
+                      <input type="radio" id="4-edits" name="rating" value="4" v-model="editratings"/>
+                      <label for="4-edits" class="star">★</label>
+                      <input type="radio" id="3-edits" name="rating" value="3" v-model="editratings"/>
+                      <label for="3-edits" class="star">★</label>
+                      <input type="radio" id="2-edits" name="rating" value="2" v-model="editratings"/>
+                      <label for="2-edits" class="star">★</label>
+                      <input type="radio" id="1-edits" name="rating" value="1" v-model="editratings" />
+                      <label for="1-edits" class="star">★</label>
+                    </div>
+                    <textarea class="form-control" id="editcomment" rows="3" v-model="editcontent"></textarea>
+                    <button type="submit" class="editbtn">
+                      <p>수정하기</p>
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
+          <!-- 버튼 누르면 수정할 수 있는 모달이 나와서 form 제출하도록 만들기 -->
         </div>
-        <!-- 버튼 누르면 수정할 수 있는 모달이 나와서 form 제출하도록 만들기 -->
-        <button v-if="comment.username == loginuser.username" @click="deleteComment(comment.id)">삭제하기</button>
       </div>
     </div>
-    <!-- {{ commentlist }} -->
+
   </div>
 </template>
 
@@ -166,18 +186,261 @@ export default {
 </script>
 
 <style>
+.rateform {
+width: 1128px;
+height: 448px;
+margin: auto;
+}
+
+.commenttitle {
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 24px;
+
+width: 1128px;
+height: 58px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.commenttitle label {
+width: 67px;
+height: 32px;
+
+/* MO/24/Bold */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 32px;
+/* identical to box height, or 133% */
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.commenttitle hr {
+width: 1128px;
+height: 2px;
+
+/* starbucks/ref/neutral/neutral87 */
+
+background: #DBDAD3;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+}
+
+.rateform button {
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 12px 24px;
+gap: 6px;
+margin-top: 40px;
+margin-left: 1025px;
+
+width: 104px;
+height: 46px;
+
+background: #3C4043;
+/* Shadow/XSM */
+
+box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.04);
+border-radius: 100px;
+}
+
+.rateform p {
+width: 70px;
+height: 22px;
+
+/* Text Button/Semibold Large */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 15px;
+line-height: 22px;
+/* identical to box height, or 147% */
+
+
+/* Base/White */
+
+color: #FFFFFF;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.commentcontext {
+width: 1128px;
+height: 264px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+
 #comment {
-  width: 95%;
-  margin: auto;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  resize: none;
+box-sizing: border-box;
+
+/* Auto layout */
+
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 24px 16px;
+gap: 8px;
+margin-top: 20px;
+
+width: 1128px;
+height: 200px;
+
+/* starbucks/white */
+
+background: #FFFFFF;
+/* starbucks/ref/neutral/neutral87 */
+
+border: 1px solid #DBDAD3;
+/* Shadow/XSM */
+
+box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.04);
+border-radius: 24px;
+}
+
+.editcomment {
+box-sizing: border-box;
+
+/* Auto layout */
+
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 24px 16px;
+gap: 8px;
+margin-top: 20px;
+
+width: 450px;
+height: 200px;
+
+/* starbucks/white */
+
+background: #FFFFFF;
+/* starbucks/ref/neutral/neutral87 */
+
+border: 1px solid #DBDAD3;
+/* Shadow/XSM */
+
+box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.04);
+border-radius: 24px;
 }
 
 .commentform {
-  border: solid;
-  width: 800px;
-  padding: 10px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+padding: 0px;
+gap: 40px;
+
+width: 1128px;
+height: 362px;
+}
+
+.commentlist {
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 24px;
+}
+
+.cltitle {
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 24px;
+margin: auto;
+
+width: 1128px;
+height: 58px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.cltitle p {
+width: 138px;
+height: 32px;
+
+/* MO/24/Bold */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 32px;
+/* identical to box height, or 133% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.cltitle hr {
+
+width: 1128px;
+height: 2px;
+
+/* starbucks/ref/neutral/neutral87 */
+
+background: #DBDAD3;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
 }
 
 .star-rating {
@@ -189,6 +452,7 @@ export default {
   padding: 0 0.2em;
   text-align: center;
   width: 5em;
+  margin-left: 0 !important;
 }
  
 .star-rating input {
@@ -212,9 +476,218 @@ export default {
 }
 
 .comment {
-  border: solid;
+  /* border: solid;
   width: 800px;
-  margin-bottom: 20px;
+  margin-bottom: 20px; */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 40px 24px;
+gap: 16px;
+margin: auto;
+
+width: 1128px;
+height: 144px;
+
+/* starbucks/sys/light/surface-container-high */
+
+background: #E9E8E1;
+border-radius: 24px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
 }
 
+.comment p {
+  margin-bottom: 0 !important;
+  width: 338px;
+height: 24px;
+
+/* MO/16/Regular */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+/* identical to box height, or 150% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+
+.commentone {
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 0px;
+gap: 16px;
+
+width: 158px;
+height: 24px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.commenttwo {
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 0px;
+gap: 16px;
+
+width: 864px;
+height: 43px;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+}
+
+.commenttwo p {
+width: 338px;
+height: 24px;
+
+/* MO/16/Regular */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+/* identical to box height, or 150% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+/* identical to box height, or 133% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.commenttwo button {
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 12px 18px;
+gap: 6px;
+
+width: 117px;
+height: 43px;
+
+/* background: #3C4043; */
+/* Shadow/XSM */
+
+box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.04);
+border-radius: 100px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;  
+}
+.commentone a {
+width: 92px;
+height: 22px;
+
+/* MO/16/Bold */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 21px;
+/* identical to box height, or 133% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+.commentone p {
+width: 50px;
+height: 24px;
+
+/* MO/16/Regular */
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+/* identical to box height, or 150% */
+
+
+/* starbucks/ref/neutral/neutral20 */
+
+color: #30312C;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+
+.editbtn {
+margin-top: 40px;
+
+width: 104px;
+height: 46px;
+
+background: #3C4043;
+/* Shadow/XSM */
+
+box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.04);
+border-radius: 100px;
+}
+
+.editbtn p {
+color: #FFFFFF;
+
+}
 </style>
